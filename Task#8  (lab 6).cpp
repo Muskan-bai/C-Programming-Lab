5adee13740bccc10bc3cd7b4dd47496b921654d2
Muskan-bai <K250634@nu.edu.pk>
2025-10-09 19:27:57 +0500

    lab#8
    added to the new file

---
+++ Task#8  (lab 6).cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main () {
+	int num,i;
+		printf("Enter a number: ");
+		scanf("%d", &num);
+		
+	for(i=1; i<=10; i++) {
+		
+		printf("%d X %d = %d\n",i, num, i*num);
+	}
+	return 0;
+}
