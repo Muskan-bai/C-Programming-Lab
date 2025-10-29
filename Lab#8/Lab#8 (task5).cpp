d39789380674a002399964aaca224453747b32ee
Muskan-bai <K250634@nu.edu.pk>
2025-10-29 14:22:39 +0500

    Task#5

---
+++ Lab#8/Lab#8 (task5).cpp
@@ -0,0 +1,36 @@
+#include <stdio.h>
+ int main() {
+ int i,j,n,k=0,space;
+ 	printf("Enter the number of rows: ");
+ 	scanf("%d",&n);
+ 	
+ 	for(i=1; i<=n; i++,k=0) {
+ 		
+ 	  for(space=1; space<=(n-i); space++) {
+	 		printf("  ");
+		 }
+		 
+		while (k != 2 * i - 1) {
+        	printf("* ");
+         	++k;
+      }
+		printf("\n"); 	
+	 }
+	 
+	 for(i=n-1; i>=1; i--,k=0) {
+ 		
+ 	  for(space=1; space<=(n-i); space++) {
+	 		printf("  ");
+		 }
+		 
+		while (k != 2 * i - 1) {
+        	printf("* ");
+         	++k;
+      }
+		printf("\n"); 	
+	 }
+	 
+	 
+	 
+	 return 0;
+}
